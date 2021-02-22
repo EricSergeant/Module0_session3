553858417f0ae99710517feac4bdadd213a055b4
Eric Sergeant <eric.sergeant.dev@gmail.com>
2021-02-22 14:18:09 -0600

    Add first six variables

---
+++ variablePractice.js
@@ -0,0 +1,6 @@
+var purpose = "To complete session 2 exerices";
+var variableNamingAttemps = 1;
+var pensOnDesk = ["red", "black", "blue"];
+var isDone = 0;
+var date = "Feb 22, 2021";
+var errorsFound = 0;
